8a247d5dfa13da99a86eef7665e7bc83769377a4
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-22 12:14:01 +0200

    Relationship Filtering

--- src/app.ts
+++ src/app.ts
@@ -1,152 +1,123 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import morgan from 'morgan';
+import morgan from "morgan";
 
 import config from "./config/config";
 const app = config.getApp();
 const prisma = new PrismaClient();
 
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
-app.use(express.json({limit:'100mb'}));
-app.use(express.urlencoded({ extended:true , limit:'100mb'}));
-
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-app.post('/', async (req,res)=>{
-  try{
-  const {email,name,age,emailUpdates} = req.body;
-  // prisma.user,createMany([])
-  const newUser = await prisma.user.create({
-    data:{
-       name,
-       email,
-       age,
-       UserPreference:{
-        create:{
-          emailUpdates
-        }
-       },     
-  },
-  // include:{
-  //   UserPreference:true
-  // },
-  select:{
-    // u either use the select or include
-    name:true,
-    UserPreference:{
-      select:{
-        id:true
-      }
-    }
-  }
-}) 
+app.post("/", async (req, res) => {
+  try {
+    const { email, name, age, emailUpdates } = req.body;
+    // prisma.user,createMany([])
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        age,
+        UserPreference: {
+          create: {
+            emailUpdates,
+          },
+        },
+      },
+      // include:{
+      //   UserPreference:true
+      // },
+      select: {
+        // u either use the select or include
+        name: true,
+        UserPreference: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
 
-  console.log(newUser);
+    console.log(newUser);
 
-  res.status(201).json({
-    message:'done',
-    data:{
-      newUser
-    }
-  })
-  }catch(err){
+    res.status(201).json({
+      message: "done",
+      data: {
+        newUser,
+      },
+    });
+  } catch (err) {
     console.log(err);
   }
-})
+});
 app.get("/:name", async (req, res) => {
   try {
-
     let data;
-
-    // const name = req.params.name
-    // const age = parseInt(req.params.age)
-    // data = await prisma.user.findUnique({
-    //   where:{
-    //     // email
-    //     age_name:{
-    //       age,
-    //       name
-    //     }
-    //   }
-    // })
-
-    const name = req.params.name
-    // data = await prisma.user.findFirst({
-    //   where:{ name,}
-     
-    // })
-
+    /*
     data = await prisma.user.findMany({
+      // where:{
+      //  UserPreference:{
+      //   emailUpdates:true
+      //  }
+      // },
+      // include:{
+      //   UserPreference:{
+      //     select:{
+      //       emailUpdates:true
+      //     }
+      //   }}
+      
       where:{
-        // name:{
-        //   // equals:'hawsstrmido',
-        //   // not:'hawsstrmido',
-        //   // in:['hawsstrmido','hawmido']
-        //   notIn:['hawsstrmido','hawmido']
-        // },
-
-        // name:'hawsstrmido',
-
-        // age:{
-        //   // lt:30
-        //   // gt:34
-
+       writtenPosts:{
+        // every:{
+        //   title:'test' // is every single post in that this person writtren, start with the title of test?
         // }
 
-        // email:{
-        //   // contains:'@gmail.com',
-        //   // endsWith:"co"
-        //   startsWith:'goudaa'
+        // none:{
+        //   title:'test' // has this user written no post
         // }
 
-        // AND:[
-        //   {email:{startsWith:'goudaa'}},
-        //   {name:'hamido'}
-        // ]
+        some:{
+          title:{startsWith:'test'} // do any of thir titles start with test
+        }
+       }
+       },
 
-        // AND:[
-        //   {email:{startsWith:'goudaa'}},
-        //   {email:{endsWith:'m'}}
-        // ]
-
-        // OR:[
-        //   {email:{startsWith:'goudaa'}},
-        //   {age:{gt:45}}
-        // ]
-
-        NOT:[
-          {email:{startsWith:'goudaa'}},
-          {age:{gt:45}}
-        ]
-        
-      },
     })
 
-
-
-    
+*/
+    data = await prisma.post.findMany({
+      where: {
+        author: {
+          is: {
+            age: 43,
+          },
+        },
+      },
+    });
 
     console.log(data);
     res.status(200).json({
       length: data.length,
-      data
+      data,
     });
   } catch (err) {
     console.log(err);
   }
 });
 
-app.delete('/',async (req,res)=>{
-  try{
+app.delete("/", async (req, res) => {
+  try {
     await prisma.user.deleteMany();
     res.status(204).json({
-      status: 'success',
+      status: "success",
       data: null,
-    })
-  }catch(err){
+    });
+  } catch (err) {
     console.log(err);
-
   }
-})
+});
